cbf0067e3f5e4704b2b6da84b9fb9edeccacb262
Jan Källman <jan.kallman@epplussoftware.com>
2020-01-27 11:33:08 +0100

    Altered code header

--- 11-ConditionalFormatting/ConditionalFormattingSample.cs
+++ 11-ConditionalFormatting/ConditionalFormattingSample.cs
@@ -1,5 +1,5 @@
 ﻿/*************************************************************************************************
-  Copyright (C) EPPlus Software AB. 
+  Required Notice: Copyright (C) EPPlus Software AB. 
   This software is licensed under PolyForm Noncommercial License 1.0.0 
   and may only be used for noncommercial purposes 
   https://polyformproject.org/licenses/noncommercial/1.0.0/
